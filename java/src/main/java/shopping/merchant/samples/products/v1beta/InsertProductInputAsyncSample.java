f400ee68fa9263a13af8a399c001c660975d98bb
Federico D'Amato <federico.damato91@gmail.com>
2025-03-06 17:20:12 +0100

    Update InsertProductInputAsyncSample.java

--- java/src/main/java/shopping/merchant/samples/products/v1beta/InsertProductInputAsyncSample.java
+++ java/src/main/java/shopping/merchant/samples/products/v1beta/InsertProductInputAsyncSample.java
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 package shopping.merchant.samples.products.v1beta;
-// [START merchantapi_insert_product_input]
+// [START merchantapi_insert_product_input_async]
 import com.google.api.core.ApiFuture;
 import com.google.api.core.ApiFutureCallback;
 import com.google.api.core.ApiFutures;
@@ -159,4 +159,4 @@ public class InsertProductInputAsyncSample {
     asyncInsertProductInput(config, dataSource);
   }
 }
-// [END merchantapi_insert_product_input]
+// [END merchantapi_insert_product_input_async]
