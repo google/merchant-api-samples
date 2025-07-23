823988e715cfdba7602554bef84f8f85abdc7497
Federico D'Amato <damaz@google.com>
2025-07-23 08:47:24 +0000

    No public description
    PiperOrigin-RevId: 786188966

--- java/src/main/java/shopping/merchant/samples/productsdatasourcesworkflow/v1beta/CreateDatasourceAndInsertFirstProductSample.java
+++ java/src/main/java/shopping/merchant/samples/productsdatasourcesworkflow/v1beta/CreateDatasourceAndInsertFirstProductSample.java
@@ -13,8 +13,9 @@
 // limitations under the License.
 
 package shopping.merchant.samples.productsdatasourcesworkflow.v1beta;
+
 // [START merchantapi_create_datasource_and_insert_first_product]
-import shopping.merchant.samples.datasources.v1beta.CreatePrimaryProductDataSourceWildCardSample;
+import shopping.merchant.samples.datasources.v1beta.CreatePrimaryProductDataSourceMultipleLanguagesSample;
 import shopping.merchant.samples.products.v1beta.InsertProductInputSample;
 import shopping.merchant.samples.utils.Config;
 
@@ -27,7 +28,7 @@ public class CreateDatasourceAndInsertFirstProductSample {
     // Insert the wildcard datasource that accepts all feedLabel contentLanguage combinations
     // and store its `name` to use to insert the product.
     String dataSourceName =
-        CreatePrimaryProductDataSourceWildCardSample.createDataSource(config, displayName);
+        CreatePrimaryProductDataSourceMultipleLanguagesSample.createDataSource(config, displayName);
     // Products can be inserted immediately after the datasource is created.
     InsertProductInputSample.insertProductInput(config, dataSourceName);
   }
