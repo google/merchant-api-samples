fcdf0e0c843e8f3eed37b6d38b1ba1f707ddb020
Maria Kiss Vajdova <mvajdova@google.com>
2025-10-10 08:04:54 +0000

    No public description
    PiperOrigin-RevId: 817525450

--- golang/examples/accounts/accounts/v1/get_account.go
+++ golang/examples/accounts/accounts/v1/get_account.go
@@ -1,5 +1,21 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package v1 provides examples for retrieving account information using the Merchant API.
 package v1
+
+// [START merchantapi_get_account]
 
 import (
 	"context"
@@ -53,3 +69,5 @@ func (s *getAccount) Execute() error {
 	fmt.Printf("Successfully retrieved account: %+v\n", account)
 	return nil
 }
+
+// [END merchantapi_get_account]
