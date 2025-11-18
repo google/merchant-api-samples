16923a93a12bcdb19197e88366ffb7975dee06c3
Maria Kiss Vajdova <mvajdova@google.com>
2025-11-18 13:16:40 +0000

    No public description
    PiperOrigin-RevId: 833773270

--- php/examples/products/v1/GetProductSample.php
+++ php/examples/products/v1/GetProductSample.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2024 Google LLC
+ * Copyright 2025 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,39 +15,64 @@
  * limitations under the License.
  */
 
+// [START merchantapi_get_product]
 require_once __DIR__ . '/../../../vendor/autoload.php';
 require_once __DIR__ . '/../../Authentication/Authentication.php';
 require_once __DIR__ . '/../../Authentication/Config.php';
-// [START merchantapi_get_product]
-use Google\ApiCore\ApiException;
-use Google\Shopping\Merchant\Products\V1\GetProductRequest;
-use Google\Shopping\Merchant\Products\V1\Product;
-use Google\Shopping\Merchant\Products\V1\Client\ProductsServiceClient;
 
+use Google\ApiCore\ApiException;
+use Google\Shopping\Merchant\Products\V1\Client\ProductsServiceClient;
+use Google\Shopping\Merchant\Products\V1\GetProductRequest;
 
 /**
- * This class demonstrates how to get a single product for a given Merchant Center
- * account.
+ * This class demonstrates how to get a single product for a given Merchant
+ * Center account.
  */
-class GetProduct
+class GetProductSample
 {
-
-    // ENSURE you fill in the product ID for the sample to work.
-    private const PRODUCT = 'INSERT_PRODUCT_ID_HERE';
+    /**
+     * A helper function to create the product name string.
+     *
+     * @param string $accountId
+     *      The account that owns the product.
+     * @param string $productId
+     *      The ID of the product.
+     *
+     * @return string The name has the format: `accounts/{account}/products/{product}`
+     */
+    private static function getName(string $accountId, string $productId): string
+    {
+        return sprintf("accounts/%s/products/%s", $accountId, $productId);
+    }
 
     /**
-     * Gets a product from your Merchant Center account.
+     * Encodes a string to base64url without padding. This is needed if the
+     * product ID contains special characters (such as forward slashes) and
+     * needs base64url encoding.
      *
-     * @param array  $config
-     *      The configuration data used for authentication and getting the acccount
-     *      ID.
-     * @param string $product
-     *      The product ID to get.
-     *      Format: `accounts/{account}/products/{productId}`.
+     * @param string $productId
+     *      The ID of the product.
+     *
+     * @return string The encoded product ID.
+     */
+    public static function encodeProductId(string $productId): string
+    {
+        return rtrim(strtr(base64_encode($productId), '+/', '-_'), '=');
+    }
+
+    /**
+     * Retrieves a product from your Merchant Center account.
+     *
+     * @param array $config
+     *      The configuration data used for authentication and getting the
+     *      account ID.
+     * @param string $productId
+     *      The ID of the product, in the form of
+     *      `contentLanguage:feedLabel:offerId`.
      *
      * @return void
      */
-    public static function getProductSample($config, $product) : void
+    public static function getProduct(array $config, string $productId): void
     {
         // Gets the OAuth credentials to make the request.
         $credentials = Authentication::useServiceAccountOrTokenFile();
@@ -58,23 +83,23 @@ class GetProduct
         // Creates a client.
         $productsServiceClient = new ProductsServiceClient($options);
 
+        // The name has the format: accounts/{account}/products/{productId}
+        $name = self::getName($config['accountId'], $productId);
+
+        // Creates the request.
+        $request = new GetProductRequest([
+            'name' => $name
+        ]);
+
+        // Calls the API and catches and prints any network failures/errors.
         try {
-            // Creates the request.
-            $request = new GetProductRequest(
-                [
-                    'name' => $product
-                ]
-            );
-
-            // Sends the request.
-            echo "Sending get Product request\n";
+            print "Sending get product request:\n";
             $response = $productsServiceClient->getProduct($request);
-
-            // Outputs the response.
-            echo "Retrieved Product below\n";
-            print_r($response);
-        } catch (Exception $e) {
-            echo $e->getMessage();
+            print "Retrieved Product below\n";
+            // Pretty-prints the JSON representation of the response.
+            print $response->serializeToJsonString(true) . "\n";
+        } catch (ApiException $e) {
+            printf("Call failed with message: %s\n", $e->getMessage());
         }
     }
 
@@ -86,19 +111,22 @@ class GetProduct
     public function callSample(): void
     {
         $config = Config::generateConfig();
-        // Identifies the product to retrieve.
-        // The name has the format: accounts/{account}/products/{productId}.
-        $product = ProductsServiceClient::productName(
-            $config['accountId'],
-            self::PRODUCT
-        );
 
-        self::getProductSample($config, $product);
+        // The name of the `product`, returned after a `Product.insert` request.
+        // We recommend having stored this value in your database to use for all
+        // future requests.
+        $productId = 'en~US~sku123'; // Replace with your actual product ID
+
+        // Uncomment the following line if the product name contains special
+        // characters (such as forward slashes) and needs base64url encoding.
+        // $productId = self::encodeProductId($productId);
+
+        self::getProduct($config, $productId);
     }
-
 }
 
-
-$sample = new GetProduct();
+// Runs the sample.
+$sample = new GetProductSample();
 $sample->callSample();
 // [END merchantapi_get_product]
+```
